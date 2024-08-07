86b6d8cb2222214ddbc49946a661d79ce0124ca0
Eduardomrqzhdz <Eduardomrqzhdz@users.noreply.github.com>
2024-08-07 13:48:06 -0600

    Actualización de archivos y creación de componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Titulo</h1>
-      <p>Texto de prueba</p>
-    </div>
+    <>
+      
+      <Header/>
+
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+<nav>
+<img src="" alt="Logo Little Lemon"></img>
+
+<ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Menú</a></li>
+    <li><a href="">Order online</a></li>
+    <li><a href="">Login</a></li>
+</ul>
+</nav>
